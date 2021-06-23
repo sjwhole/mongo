0631cd2f33152a786ebe8261b642bae631eb8da5
sjwhole <bluemoon2718@naver.com>
2021-06-23 18:04:04 +0900

    feat: Separate book router

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
@@ -15,12 +16,10 @@ mongoose.connect("mongodb://localhost/mongodb_tutorial");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const router = require("./routes");
+app.use("/", router);
+
 var port = process.env.PORT || 8080;
-
-var Book = require("./models/book");
-
-var router = require("./routes/index");
-router(app, Book);
 
 var server = app.listen(port, function () {
   console.log("Express server has started on port " + port);
--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,8 @@
-module.exports = function (app, Book) {
-  // GET ALL BOOKS
-  app.get("/api/books", function (req, res) {
-    Book.find((err, books) => {
-      if (err) return res.status(400).send({ error: "database failure" });
-      res.json(books);
-    });
-  });
+const express = require("express");
+const router = express.Router();
 
-  // GET SINGLE BOOK
-  app.get("/api/books/:book_id", function (req, res) {
-    Book.findOne({ _id: req.params.book_id }, (err, book) => {
-      if (err) return res.status(400).send({ error: "Bad request" });
-      res.json(book);
-    });
-  });
+const books = require("./books");
 
-  // GET BOOK BY AUTHOR
-  app.get("/api/books/author/:author", function (req, res) {
-    Book.findOne({ author: req.params.author }, (err, book) => {
-      if (err) return res.status(400).send({ error: "Bad request" });
-      res.json(book);
-    });
-  });
+router.use("/books", books);
 
-  // CREATE BOOK
-  app.post("/api/books", function (req, res) {
-    var book = new Book();
-    book.title = req.body.name;
-    book.author = req.body.author;
-    book.published_date = new Date(req.body.published_date);
-
-    book.save((err) => {
-      if (err) {
-        console.error(err);
-        res.json({ result: 0 });
-        return;
-      }
-      res.json({ result: 1 });
-    });
-  });
-
-  // UPDATE THE BOOK
-  app.put("/api/books/:book_id", function (req, res) {
-    Book.findByIdAndUpdate(
-      req.params.book_id,
-      { $set: req.body },
-      function (err) {
-        if (err) return res.status(500).json({ error: "database failure" });
-        res.json({ message: "book updated" });
-      }
-    );
-  });
-
-  // DELETE BOOK
-  app.delete("/api/books/:book_id", function (req, res) {
-    Book.findByIdAndRemove(req.params.book_id, function (err) {
-      if (err) return res.status(500).json({ error: "database failure" });
-      res.json({ message: "book deleted" });
-    });
-  });
-};
+module.exports = router;
